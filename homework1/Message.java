5ef3ff5261150d70ffa4fb6ab9f1de8c906ebfce
Selim Aynigül <98275022+selimaynigul@users.noreply.github.com>
2023-06-20 18:27:20 +0300

    Add files via upload

---
+++ homework1/Message.java
@@ -0,0 +1,44 @@
+/**
+ * @author Selim Aynigül
+ */
+
+package homework1;
+
+public class Message {
+
+    private int messageId;
+    private int senderId;
+    private int receiverId;
+    private String content;
+
+    /**
+     * 
+     * @param messageId 
+     * @param senderId
+     * @param receiverId
+     * @param content
+     */
+    public Message(int messageId, int senderId, int receiverId, String content) {
+        this.messageId = messageId;
+        this.senderId = senderId;
+        this.receiverId = receiverId;
+        this.content = content;
+    }
+
+    public int getMessageId() {
+        return messageId;
+    }
+    
+    public int getSenderId() {
+        return senderId;
+    }
+
+    public int getReceiverId() {
+        return receiverId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
